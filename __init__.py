7c1b99c06e4fa3e0d9ffc539c7c5114466a18595
yang0 <yang0@126.com>
2025-04-23 10:49:52 +0800

    bugfix

--- __init__.py
+++ __init__.py
@@ -1,6 +1,7 @@
 from .svg import *
 from .icon import *
-VERSION = "0.0.3"
+
+VERSION = "0.0.4"
 GIT_URL = "https://github.com/yang0/autotask_fileConvert"
 NAME = "File Format Converter"
 DESCRIPTION = "A comprehensive file format conversion plugin supporting SVG to image conversion and image to icon formats (ICO/ICNS) with high-quality output and multiple size variants"
